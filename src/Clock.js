f2c7549d6069a593608a042e7c7e19f645b28abb
Duncan Cheruiyot Langat <duncan.langat@student.moringaschool.com>
2022-09-26 07:27:52 +0300

    Completed assignment

--- src/Clock.js
+++ src/Clock.js
@@ -1,12 +1,19 @@
+
+
 import React, { useState, useEffect } from "react";
 
 function Clock() {
   const [time, setTime] = useState(new Date());
 
   useEffect(() => {
-    setInterval(() => {
+    const timerInterval = setInterval(() => {
       setTime(new Date());
     }, 1000);
+
+    // returning a cleanup function
+    return function cleanup() {
+      clearInterval(timerInterval);
+    };
   }, []);
 
   return <div>{time.toString()}</div>;
